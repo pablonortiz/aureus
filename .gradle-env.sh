cab6bc098a049afe23389d55b4a6c57e7fc9bc4d
Pablo Nicolás Ortiz <pablonortiz05@hotmail.com>
2026-02-26 01:02:21 -0300

    fix: remove all hardcoded local paths from tracked files
    - Remove nodeExecutableAndArgs from build.gradle (uses PATH default)
    - Untrack and gitignore .gradle-env.sh (local env only)

--- .gradle-env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export PATH="/Users/pablonortiz/.nvm/versions/node/v22.22.0/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/pablonortiz/.sdkman/candidates/java/17.0.12-oracle/bin:/Users/pablonortiz/Library/Android/sdk/platform-tools"
-export JAVA_HOME="/Users/pablonortiz/.sdkman/candidates/java/17.0.12-oracle"
-export ANDROID_HOME="/Users/pablonortiz/Library/Android/sdk"
-exec "$@"
